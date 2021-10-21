0755d5215bc545ee75b801964602276dfc1eebef
Scott Gorby <scott.gorby15@pcc.edu>
2021-10-21 09:07:35 -0700

    added comments for main and test classes

--- src/edu/pcc/cis/goodbyeworld/GoodbyeWorldTest.java
+++ src/edu/pcc/cis/goodbyeworld/GoodbyeWorldTest.java
@@ -1,3 +1,10 @@
+/**
+ * This class tests the goodbyeworld class
+ *
+ * @author Scott J. Gorby
+ * @version 2021.10.21
+ */
+
 package edu.pcc.cis.goodbyeworld;
 
 import static org.junit.Assert.*;
