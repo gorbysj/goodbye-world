6fc81cc850653e76ad21d467254cd89089840885
Scott Gorby <scott.gorby15@pcc.edu>
2021-10-21 08:23:21 -0700

    added main method that prints Goodbye World!

--- src/edu/pcc/cis/goodbyeworld/GoodbyeWorld.java
+++ src/edu/pcc/cis/goodbyeworld/GoodbyeWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.goodbyeworld;
  * Prints Goodbye World!
  */
 public class GoodbyeWorld {
+    public static void main(String[] args) {
+        System.out.println("Goodbye World!");
+    }
 }
