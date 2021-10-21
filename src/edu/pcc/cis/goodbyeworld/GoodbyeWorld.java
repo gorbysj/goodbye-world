d298dcc3eace604dd38ad0173094ec700d53df42
Scott Gorby <scott.gorby15@pcc.edu>
2021-10-21 08:26:10 -0700

    print date

--- src/edu/pcc/cis/goodbyeworld/GoodbyeWorld.java
+++ src/edu/pcc/cis/goodbyeworld/GoodbyeWorld.java
@@ -1,10 +1,12 @@
 package edu.pcc.cis.goodbyeworld;
 
+import java.util.Date;
 /**
  * Prints Goodbye World!
  */
 public class GoodbyeWorld {
     public static void main(String[] args) {
         System.out.println("Goodbye World!");
+        System.out.println(new Date());
     }
 }
